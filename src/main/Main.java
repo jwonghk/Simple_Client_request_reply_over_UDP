69cf0a8b216e82b3bbf5395d74a3d05a7b64760a
Ocean <johnnwong@hotmail.com>
2023-01-13 12:51:43 -0800

    Finishing main functions: with Client_UDP added

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,12 @@ public class Main {
         int studID;
         studID = parseInt(args[0]);
 
+        bytes[0] = (byte) (studID & 0xFF);
+        bytes[1] = (byte) ((byte) (studID >> 8) & 0XFF);
+        bytes[2] = (byte) ((byte) (studID >> 16) & 0XFF);
+        bytes[3] = (byte) ((byte) (studID >> 24) & 0XFF);
+
+
         //byte[0] = args[0];
 
         /*for (String str : args) {
